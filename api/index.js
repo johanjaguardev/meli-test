641724864fab30e8292cb5d8657092cd10c59401
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-11-01 18:16:47 -0500

    Second get and post

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+
 app.get("/", function (req, res) {
   res.send("Saludos desde Express");
 });
+
+app.get("/hola", function (req, res) {
+  res.send("Hola desde Express");
+});
+
+app.post("/hola", function (req, res) {
+  res.send("Post Saludos desde Express");
+});
+
 app.listen(3000, () => {
   console.log("El servidor esta inicializado en el puerto 3000");
 });
