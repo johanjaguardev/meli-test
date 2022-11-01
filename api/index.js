ab1b7f9cc9fa0aba0b1add31b6394525e29e1338
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-11-01 18:56:38 -0500

    Usuario Example rest Api Node + Express

--- api/index.js
+++ api/index.js
@@ -1,18 +1,131 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let usuario = {
+  nombre: "",
+  apellido: "",
+};
+
+let respuesta = {
+  error: false,
+  codigo: 200,
+  mensaje: "",
+};
 
 app.get("/", function (req, res) {
-  res.send("Saludos desde Express");
+  respuesta = {
+    error: true,
+    codigo: 200,
+    mensaje: "Punto de Inicio",
+  };
+  res.send(respuesta);
+});
+app.get("/usuario", function (req, res) {
+  respuesta = {
+    error: false,
+    codigo: 200,
+    mensaje: "respuesta de usuario",
+    respuesta: usuario,
+  };
+
+  if (usuario.nombre === "" || usuario.apellido === "") {
+    respuesta = {
+      error: false,
+      codigo: 200,
+      mensaje: "",
+    };
+  }
+  res.send(respuesta);
 });
 
-app.get("/hola", function (req, res) {
-  res.send("Hola desde Express");
+app.post("/usuario", function (req, res) {
+  usuario = {
+    nombre: req.body.nombre,
+    apellido: req.body.apellido,
+  };
+  respuesta = {
+    error: false,
+    codigo: 200,
+    mensaje: "Usuario creado",
+    respuesta: usuario,
+  };
+
+  if (!req.body.nombre || !req.body.apellido) {
+    respuesta = {
+      error: true,
+      codigo: 502,
+      mensaje: "El campo nombre y apellido son requeridos",
+    };
+  } else {
+    if (usuario.nombre !== "" || usuario.apellido !== "") {
+      respuesta = {
+        error: true,
+        codigo: 503,
+        mensaje: "El usuario ya fue creado previamente",
+      };
+    }
+  }
+  res.send(respuesta);
+});
+app.put("/usuario", function (req, res) {
+  usuario = {
+    nombre: req.body.nombre,
+    apellido: req.body.apellido,
+  };
+  respuesta = {
+    error: false,
+    codigo: 200,
+    mensaje: "Usuario actualizado",
+    respuesta: usuario,
+  };
+  if (!req.body.nombre || !req.body.apellido) {
+    respuesta = {
+      error: true,
+      codigo: 502,
+      mensaje: "El campo nombre y apellido son requeridos",
+    };
+  } else {
+    if (usuario.nombre === "" || usuario.apellido === "") {
+      respuesta = {
+        error: true,
+        codigo: 501,
+        mensaje: "El usuario no ha sido creado",
+      };
+    }
+  }
+  res.send(respuesta);
 });
 
-app.post("/hola", function (req, res) {
-  res.send("Post Saludos desde Express");
+app.delete("/usuario", function (req, res) {
+  respuesta = {
+    error: false,
+    codigo: 200,
+    mensaje: "Usuario eliminado",
+  };
+  usuario = {
+    nombre: "",
+    apellido: "",
+  };
+  if (usuario.nombre === "" || usuario.apellido === "") {
+    respuesta = {
+      error: true,
+      codigo: 501,
+      mensaje: "El usuario no ha sido creado",
+    };
+  }
+  res.send(respuesta);
 });
-
+app.use(function (req, res, next) {
+  respuesta = {
+    error: true,
+    codigo: 404,
+    mensaje: "URL no encontrada",
+  };
+  res.status(404).send(respuesta);
+});
 app.listen(3000, () => {
   console.log("El servidor esta inicializado en el puerto 3000");
 });
