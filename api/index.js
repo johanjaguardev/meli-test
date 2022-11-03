c40f70cddc2154f2ae21be4ed6b6c6d526d375df
Johan Vargas Sánchez <johanvargasdev@gmail.com>
2022-11-03 14:19:11 -0500

    2.1 using env variables to set up name and lastname for author

--- api/index.js
+++ api/index.js
@@ -1,132 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+const fs = require("fs");
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const routes = require("./routes/routes.js")(app, fs);
 
-let usuario = {
-  nombre: "",
-  apellido: "",
-};
-
-let respuesta = {
-  error: false,
-  codigo: 200,
-  mensaje: "",
-};
-
-app.get("/", function (req, res) {
-  respuesta = {
-    error: true,
-    codigo: 200,
-    mensaje: "Punto de Inicio",
-  };
-  res.send(respuesta);
-});
-
-app
-  .route("/usuario")
-  .get(function (req, res) {
-    respuesta = {
-      error: false,
-      codigo: 200,
-      mensaje: "respuesta de usuario",
-      respuesta: usuario,
-    };
-
-    if (usuario.nombre === "" || usuario.apellido === "") {
-      respuesta = {
-        error: false,
-        codigo: 200,
-        mensaje: "",
-      };
-    }
-    res.send(respuesta);
-  })
-  .post(function (req, res) {
-    usuario = {
-      nombre: req.body.nombre,
-      apellido: req.body.apellido,
-    };
-    respuesta = {
-      error: false,
-      codigo: 200,
-      mensaje: "Usuario creado",
-      respuesta: usuario,
-    };
-
-    if (!req.body.nombre || !req.body.apellido) {
-      respuesta = {
-        error: true,
-        codigo: 502,
-        mensaje: "El campo nombre y apellido son requeridos",
-      };
-    } else {
-      if (usuario.nombre !== "" || usuario.apellido !== "") {
-        respuesta = {
-          error: true,
-          codigo: 503,
-          mensaje: "El usuario ya fue creado previamente",
-        };
-      }
-    }
-    res.send(respuesta);
-  })
-  .put(function (req, res) {
-    usuario = {
-      nombre: req.body.nombre,
-      apellido: req.body.apellido,
-    };
-    respuesta = {
-      error: false,
-      codigo: 200,
-      mensaje: "Usuario actualizado",
-      respuesta: usuario,
-    };
-    if (!req.body.nombre || !req.body.apellido) {
-      respuesta = {
-        error: true,
-        codigo: 502,
-        mensaje: "El campo nombre y apellido son requeridos",
-      };
-    } else {
-      if (usuario.nombre === "" || usuario.apellido === "") {
-        respuesta = {
-          error: true,
-          codigo: 501,
-          mensaje: "El usuario no ha sido creado",
-        };
-      }
-    }
-    res.send(respuesta);
-  })
-  .delete(function (req, res) {
-    respuesta = {
-      error: false,
-      codigo: 200,
-      mensaje: "Usuario eliminado",
-    };
-    usuario = {
-      nombre: "",
-      apellido: "",
-    };
-    if (usuario.nombre === "" || usuario.apellido === "") {
-      respuesta = {
-        error: true,
-        codigo: 501,
-        mensaje: "El usuario no ha sido creado",
-      };
-    }
-    res.send(respuesta);
-  });
-app.use(function (req, res, next) {
-  respuesta = {
-    error: true,
-    codigo: 404,
-    mensaje: "URL no encontrada",
-  };
-  res.status(404).send(respuesta);
-});
-app.listen(3000, () => {
-  console.log("El servidor esta inicializado en el puerto 3000");
+console.log(process.env.AUTHOR_NAME, process.env.AUTHOR_LASTNAME);
+debugger;
+const server = app.listen(3000, () => {
+  console.log("listening on port %s...", server.address().port);
 });
